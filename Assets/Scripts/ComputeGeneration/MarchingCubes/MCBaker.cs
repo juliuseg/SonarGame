1698d72335a1ecc8180938e574845d50345ceadb
s204427 <s204427@student.dtu.dk>
2025-09-01 23:41:37 +0200

    Damned MC WORKING

--- Assets/Scripts/ComputeGeneration/MarchingCubes/MCBaker.cs
+++ Assets/Scripts/ComputeGeneration/MarchingCubes/MCBaker.cs
@@ -22,12 +22,14 @@ public class MCBaker : MonoBehaviour
     
 
     [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
-    private struct MCVertex {
-        public Vector3 position;
+    private struct MCTriangle {
+        public Vector3 position0;
+        public Vector3 position1;
+        public Vector3 position2;
 
     }
     
-    private const int VERT_STRIDE = sizeof(float) * (3);
+    private const int TRIANGLE_STRIDE = sizeof(float) * (3*3);
 
     
     
@@ -60,24 +62,29 @@ public class MCBaker : MonoBehaviour
         if (maxVertices == 0) { generatedMesh = null; return false; }
 
         // Append buffer + counter
-        var vertBuffer = new ComputeBuffer(maxVertices, VERT_STRIDE, ComputeBufferType.Append);
-        vertBuffer.SetCounterValue(0);
+        var triangleBuffer = new ComputeBuffer(maxVertices, TRIANGLE_STRIDE, ComputeBufferType.Append);
+        triangleBuffer.SetCounterValue(0);
 
         int idMCKernel = shader.FindKernel("Main");
 
-        // Set buffers and variables
-        shader.SetBuffer(idMCKernel, "_GeneratedVertices", vertBuffer);
+        // Set buffers and variables 
+        shader.SetBuffer(idMCKernel, "_GeneratedTriangles", triangleBuffer);
     
-        Vector3 origin = transform.position - new Vector3(settings.scale.x / 2, 0, settings.scale.z / 2);
+        Vector3 origin = transform.position - new Vector3(settings.scale.x / 2, settings.scale.y / 2, settings.scale.z / 2);
         Vector3 scale = settings.scale;
-        scale.x /= settings.chunkDims.x;
-        scale.y /= settings.chunkDims.y;
-        scale.z /= settings.chunkDims.z;
 
         // Convert the scale and rotation settings into a transformation matrix
         shader.SetMatrix("_Transform", Matrix4x4.TRS(origin, Quaternion.Euler(Vector3.zero), scale));
         shader.SetFloat("_IsoLevel", settings.isoLevel);
         shader.SetInts("_ChunkDims", settings.chunkDims.x, settings.chunkDims.y, settings.chunkDims.z);
+        shader.SetFloats("_NoiseOffset", settings.noiseOffset.x,
+                                   settings.noiseOffset.y,
+                                   settings.noiseOffset.z);
+
+        shader.SetFloats("_NoiseFrequency", settings.noiseFrequency.x,
+                                            settings.noiseFrequency.y,
+                                            settings.noiseFrequency.z);
+
 
 
         shader.GetKernelThreadGroupSizes(idMCKernel, out uint tgX, out uint tgY, out uint tgZ);
@@ -90,40 +97,42 @@ public class MCBaker : MonoBehaviour
 
         // Read back only the appended count
         var countBuf = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.Raw);
-        ComputeBuffer.CopyCount(vertBuffer, countBuf, 0);
+        ComputeBuffer.CopyCount(triangleBuffer, countBuf, 0);
         uint[] countArr = { 0 };
         countBuf.GetData(countArr);
-        int vertCount = (int)countArr[0];
+        int triangleCount = (int)countArr[0];
 
-        var verts = new MCVertex[vertCount];
-        if (vertCount > 0) vertBuffer.GetData(verts, 0, 0, vertCount);
+        var triangles = new MCTriangle[triangleCount];
+        if (triangleCount > 0) triangleBuffer.GetData(triangles, 0, 0, triangleCount);
 
-        // print the first 12 vertices
-        for (int i = 0; i < 12; i++) {
-            Debug.Log($"Vertex {i}: {verts[i].position}");
-        }
 
-        generatedMesh = ComposeMesh(verts);
+        generatedMesh = ComposeMesh(triangles);
 
         countBuf.Release();
-        vertBuffer.Release();
+        triangleBuffer.Release();
         return true;
 
     }
 
 
-    private static Mesh ComposeMesh(MCVertex[] verts) {
+    private static Mesh ComposeMesh(MCTriangle[] triangles) {
         var mesh = new Mesh { indexFormat = IndexFormat.UInt32 };
-        int n = verts.Length;
+        int n = triangles.Length;
         if (n == 0) return mesh;
 
-        var v = new Vector3[n];
-        for (int i = 0; i < n; i++) { 
-            v[i] = verts[i].position; 
-        }
+        var v = new Vector3[n * 3];
+        var indices = new int[n * 3];
 
-        var indices = new int[n];
-        for (int i = 0; i < n; i++) indices[i] = i;
+        for (int i = 0; i < n; i++) {
+            int baseIdx = i * 3;
+            v[baseIdx]     = triangles[i].position0;
+            v[baseIdx + 1] = triangles[i].position1;
+            v[baseIdx + 2] = triangles[i].position2;
+
+            indices[baseIdx]     = baseIdx;
+            indices[baseIdx + 1] = baseIdx + 1;
+            indices[baseIdx + 2] = baseIdx + 2;
+        }
 
         mesh.SetVertices(v);
         mesh.SetIndices(indices, MeshTopology.Triangles, 0, true);
@@ -131,6 +140,7 @@ public class MCBaker : MonoBehaviour
         mesh.RecalculateBounds();
         return mesh;
     }
+
 
 
 }
--- Assets/Scripts/ComputeGeneration/MarchingCubes/MCSettings.cs
+++ Assets/Scripts/ComputeGeneration/MarchingCubes/MCSettings.cs
@@ -6,4 +6,6 @@ public class MCSettings : ScriptableObject
     public Vector3 scale;
     public Vector3Int chunkDims;
     public float isoLevel;
+    public Vector3 noiseFrequency;
+    public Vector3 noiseOffset;
 }
